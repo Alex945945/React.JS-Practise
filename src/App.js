2953c16b8d02c1c2980a7bdf5972fde97307b5f9
Alexandr <warlex945@gmail.com>
2022-09-25 18:40:19 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function Result({ correct }) {
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
       <a href="/">
-      <button>Попробовать снова</button>
+        <button>Попробовать снова</button>
       </a>
     </div>
   );
